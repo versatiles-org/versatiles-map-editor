19e248eedea53964707ed498f308ba68144615e6
Michael Kreil <github@michael-kreil.de>
2025-01-26 14:06:42 +0100

    refactor: and test map_style

---
+++ src/lib/utils/map_style.ts
@@ -0,0 +1,22 @@
+import { styles, type StyleBuilderOptions } from '@versatiles/style';
+import { getLanguage } from './location.js';
+
+export function getMapStyle(darkMode: boolean, styleOptions: StyleBuilderOptions = {}) {
+	return styles.colorful({
+		baseUrl: 'https://tiles.versatiles.org',
+		language: getLanguage(),
+		recolor: {
+			invertBrightness: darkMode,
+			gamma: darkMode ? 0.5 : 1
+		},
+		...styleOptions
+	});
+}
+
+export function isDarkMode(element: HTMLElement): boolean {
+	const colorScheme = getComputedStyle(element).getPropertyValue('color-scheme');
+	if (colorScheme.includes('dark')) return true;
+	if (colorScheme.includes('light')) return false;
+
+	return window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
