e94ef2e1e0c8325ee58a1a1bc75f37debbcf4408
Michael Kreil <github@michael-kreil.de>
2026-06-01 16:02:35 +0200

    feat: replace BasicMap component with inline map implementation in MapEditor

--- src/lib/MapEditor.svelte
+++ src/lib/MapEditor.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	import 'maplibre-gl/dist/maplibre-gl.css';
+	import maplibre from 'maplibre-gl';
 	import type { Map as MaplibreMapType } from 'maplibre-gl';
-	import BasicMap from '$lib/components/BasicMap/BasicMap.svelte';
 	import Sidebar from './components/Sidebar.svelte';
 	import { getCountryBoundingBox } from '$lib/utils/location.js';
 	import { GeometryManager } from './lib/geometry_manager.js';
@@ -13,8 +14,42 @@
 		onMapLoad?: (map: MaplibreMapType, maplibre: typeof import('maplibre-gl')) => void;
 	} = $props();
 
+	let container: HTMLDivElement;
+	let map: MaplibreMapType | undefined;
+	let triggeredMapReady = false;
 	let showSidebar = $state(false);
 	let geometryManager: GeometryManager | GeometryManagerInteractive | undefined = $state();
+
+	$effect(() => {
+		if (container) init();
+	});
+
+	function init(): void {
+		if (map) return;
+
+		container.style.setProperty('--bg-color', '#fff');
+		container.style.setProperty('--fg-color', '#000');
+
+		// The editor starts without a style; geometry_manager sets the actual map style.
+		map = new maplibre.Map({
+			container,
+			renderWorldCopies: false,
+			dragRotate: false,
+			attributionControl: false,
+			fadeDuration: 0
+		});
+
+		onMapInit(map, maplibre);
+
+		map.on('idle', checkMapReady);
+
+		function checkMapReady() {
+			if (triggeredMapReady) return;
+			if (!map!.loaded()) return;
+			triggeredMapReady = true;
+			if (onMapLoad) onMapLoad(map!, maplibre);
+		}
+	}
 
 	function onMapInit(map: MaplibreMapType, maplibre: typeof import('maplibre-gl')) {
 		showSidebar = window.self === window.top;
@@ -56,13 +91,7 @@
 
 <div class="page">
 	<div class="container">
-		<BasicMap
-			{onMapInit}
-			{onMapLoad}
-			emptyStyle={true}
-			mapOptions={{ attributionControl: false }}
-			styleOptions={{ darkMode: false }}
-		></BasicMap>
+		<div class="map" bind:this={container}></div>
 	</div>
 	{#if showSidebar && geometryManager && geometryManager.isInteractive()}
 		<Sidebar {geometryManager} />
@@ -95,6 +124,28 @@
 		height: 100%;
 		position: relative;
 		min-height: 6em;
+	}
+
+	.map {
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+	}
+
+	.map :global(canvas) {
+		outline: none !important;
+	}
+	.map :global(.maplibregl-ctrl-attrib) {
+		background-color: color-mix(in srgb, var(--bg-color) 50%, transparent) !important;
+		color: var(--fg-color) !important;
+		opacity: 0.5;
+		font-size: 0.85em;
+		line-height: normal !important;
+	}
+	.map :global(.maplibregl-ctrl-attrib a) {
+		color: var(--fg-color) !important;
 	}
 
 	:global(.maplibregl-ctrl-attrib) {
